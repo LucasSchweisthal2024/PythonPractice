cbf40bcc33ec193edd50860dd577a223be48ff03
LucasSchweisthal2024 <818482@mystma.org>
2021-09-16 15:18:44 -0500

    MadLib

--- main.py
+++ main.py
@@ -1,41 +1,22 @@
+def MadLib(
+    instrument,
+    number,
+     dayWeek,
+      letter,
+       major,
+        numberTwo,
+         numberThree,
+          adjective,
+           adjectiveTwo,
+            fraction,
+            adjectiveThree,
+            nameOfIntrument,
+            adjectiveFour,
+            noun,
+            familyMember,
+            NameOfASong,
+            AjectiveFive,
+            FavoriteArtist,):
+    print("While I've stayed at home, I've practiced the " + instrument + "for " + number + "hours every " + dayWeek + ". My favorite key signature to play and practive in is " + letter + major + ". it has " + numberTwo + "flats and " + numberThree + "sharps. i like this keysignature because it is " + adjective + "and " + adjectiveTwo + ". I've also gotten awesome at counting time signatures. The time signatures i live is " + fraction + "because it is " + adjectiveThree + " to count. One instrument i want to learn how to play while i'm at home is " + nameOfIntrument + ", because it is " + adjectiveFour + " and sounds like a " + noun + ". My " + familyMember + " likes it when i play " + NameOfASong + ", and always gives me a round of applause afer my performances! When i return to school, my teacher will " + AjectiveFive + " of how great i am at playing my instrument. My teacher might even thikn i sound like " + FavoriteArtist)
 
-def topFiveMovies():
-    #priting movie
-    print("Lord of the Rings")
-
-    print("Pirates of the Caribean")
-
-    print("Ready Player One")
-
-def topThreeMonitorRefreshRates():
-    print("144")
-
-    print("240")
-
-    print("360")
-
-def topThreeClassesThisTri():
-    print("programing")
-
-    print("health")
-
-    print("band")
-
-def topThreeCheese():
-    print("motzerella")
-
-    print("american cheese")
-
-    print("cheese stick")
-
-def topThreePencilLeadSizes():
-    print("0.7")
-
-    print("i dont know any other pecnil sizes")
-
-
-def myNameIs(
-    turtle
-):
-    print(turtle + " is my name")
-myNameIs("Lucas")
+MadLib("guitar", "7", "tuesday", "L", "e", "25", "78", "running", "sitting", "1/4", "coding", "saxophone", "jumping", "mall of america", "brother", "basket case", "gaming", "fame and fire")
